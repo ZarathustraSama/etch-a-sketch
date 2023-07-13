b20ab85e0267c9471009012b1dceb9d1873d3991
ZarathustraSama <mr.gabriel@live.it>
2023-07-13 09:25:37 +0200

    Now mouse colors cells

--- script.js
+++ script.js
@@ -18,8 +18,18 @@ function createItems(rowDiv) {
     for (let i = 0; i < 16; i++) {
         const itemDiv = document.createElement('div');
         itemDiv.classList.add('flexItem');
+        itemDiv.addEventListener('mouseover', () => changeColor(itemDiv));
         rowDiv.appendChild(itemDiv);
     }
+}
+
+function changeColor(itemDiv) {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+    let randomHexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        randomHexColor += hex[Math.floor(Math.random() * hex.length)];
+    }
+    itemDiv.style.backgroundColor = randomHexColor;
 }
 
 createFlexGrid();
