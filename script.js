5ffde8bf5457951a5f19c70a61186222e76a9ad8
ZarathustraSama <mr.gabriel@live.it>
2023-07-13 10:15:15 +0200

    Grid functions now accept parameters, added button logic

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-function createFlexGrid() {
-    const flexGrid = document.createElement('div');
-    flexGrid.classList.add('flexGrid');
-    createRows(flexGrid);
-    document.querySelector('body').appendChild(flexGrid);
+function createFlexGrid(size=16) {
+    if (size < 100) {
+        const flexGrid = document.createElement('div');
+        flexGrid.classList.add('flexGrid');
+        createRows(flexGrid, rows=size);
+        document.querySelector('body').appendChild(flexGrid);
+    }
+    else return alert('Number must not exceed 100');
 }
 
-function createRows(flexGrid) {
-    for (let i = 0; i < 16; i++) {
-        const rowDiv = document.createElement('div');
-        rowDiv.classList.add('flexRow');
-        createItems(rowDiv);
-        flexGrid.appendChild(rowDiv);
+function createRows(flexGrid, rows) {
+    if (rows < 100) {
+        for (let i = 0; i < rows; i++) {
+            const rowDiv = document.createElement('div');
+            rowDiv.classList.add('flexRow');
+            createItems(rowDiv, items=rows);
+            flexGrid.appendChild(rowDiv);
+        }
     }
+    else return alert('Number must not exceed 100');
 }
 
-function createItems(rowDiv) {
-    for (let i = 0; i < 16; i++) {
-        const itemDiv = document.createElement('div');
-        itemDiv.classList.add('flexItem');
-        itemDiv.addEventListener('mouseover', () => changeColor(itemDiv));
-        rowDiv.appendChild(itemDiv);
+function createItems(rowDiv, items) {
+    if (items < 100) {
+        for (let i = 0; i < items; i++) {
+            const itemDiv = document.createElement('div');
+            itemDiv.classList.add('flexItem');
+            itemDiv.addEventListener('mouseover', () => changeColor(itemDiv));
+            rowDiv.appendChild(itemDiv);
+        }
     }
+    else return alert('Number must not exceed 100');
 }
 
 function changeColor(itemDiv) {
@@ -33,3 +42,9 @@ function changeColor(itemDiv) {
 }
 
 createFlexGrid();
+
+const changeGridButton = document.querySelector('#new-grid-size');
+changeGridButton.onclick = () => {
+    document.querySelector('body').removeChild(document.querySelector('.flexGrid'));
+    createFlexGrid(size=parseInt(prompt('Choose a new grid size:')));
+}
