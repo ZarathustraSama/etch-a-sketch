09668802434a42715e88f844d3dff30c866be3b3
ZarathustraSama <mr.gabriel@live.it>
2023-07-13 08:29:38 +0200

    Added basic DOM manipulation to create grid's divs

---
+++ script.js
@@ -0,0 +1,25 @@
+function createFlexGrid() {
+    const flexGrid = document.createElement('div');
+    flexGrid.classList.add('flexGrid');
+    createRows();
+    document.querySelector('body').appendChild(flexGrid);
+}
+
+function createRows() {
+    for (let i = 0; i < 16; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('flexRow');
+        rowDiv.id = `row${i}`;
+        createItems(i);
+        flexGrid.appendChild(rowDiv);
+    }
+}
+
+function createItems(row) {
+    const rowDiv = doument.querySelector(`row${row}`);
+    for (let i = 0; i < 16; i++) {
+        const itemDiv = document.createElement('div');
+        itemDiv.classList.add('flexItem');
+        rowDiv.appendChild(itemDiv);
+    }
+}
